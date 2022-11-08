6379e6a01ba7f5c1b68fd17a2341ec2978e6f9e3
ObjectRetros <dennisobject@gmail.com>
2022-11-08 13:16:53 +0100

    Only update auth user coords if they aren't identical

--- app/Http/Controllers/LocationController.php
+++ app/Http/Controllers/LocationController.php
@@ -13,7 +13,10 @@ class LocationController extends Controller
     public function store(LocationFormRequest $request)
     {
         $user = $request->user();
-        $user->update($request->validated());
+
+        if ($user->latitude != $request->input('latitude') || $user->longitude != $request->input('longitude')) {
+            $user->update($request->validated());
+        }
 
         return response(UserResource::collection(User::query()->select(['id', 'name', 'latitude', 'longitude'])->get()));
     }
